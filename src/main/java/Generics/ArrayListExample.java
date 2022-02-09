67e6dae4ad8a33e8c629096e68edcf6e42936b0f
Mehmet Ay <aymehmet1976@gmail.com>
2022-02-09 17:33:15 +0300

    java_8_Update_Generics

--- src/main/java/Generics/ArrayListExample.java
+++ src/main/java/Generics/ArrayListExample.java
@@ -1,4 +1,24 @@
 package Generics;
 
+import java.util.ArrayList;
+
 public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+        ArrayList items=new ArrayList();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+
+        printDouble(items);
+    }
+
+    private  static void printDouble(ArrayList items){
+
+
+
+    }
 }
